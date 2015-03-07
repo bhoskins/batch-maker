91f8303a683f7393f97a07244d84c09048b3ace3
Beth Hoskins <bhoskins@charter.net>
2015-03-07 12:40:42 -0500

    recipe controller set, recipe template helpers included.

--- app/routes/recipe.js
+++ app/routes/recipe.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // model: "new-recipe",
+
+  // setupController: function(controller, model){
+  //   controller.set('model', model.recipes);
+  //   // controller.set('users', model.users);
+  // }
+
+  controllerName: 'new-recipe'
 });
