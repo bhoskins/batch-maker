d82e5c95ac2f5d6860afd609bcdb1e883300e251
Beth Hoskins <bhoskins@charter.net>
2015-03-07 16:46:03 -0500

    edited parse col names and perfected the tempDenom selector for isF boolean

--- app/controllers/new-recipe.js
+++ app/controllers/new-recipe.js
@@ -2,40 +2,46 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     mealType: ["Breakfast", "Lunch", "Dinner", "Dessert"],
-    tempDenom: ["F", "C"],
+    currentDenom: {isF: true},
+    tempDenom: [{denom: "F", isF: true},
+    						{denom: "C", isF: false}],
     unitOfMeasure: ["Cups", "Pints", "Lbs", "Oz", "Litres"],
+    recipeYieldType: ["Cookies", "Loaf/Loaves/Cake/s", "items", "Servings"],
 	actions: {
 		saveRecipe: function() {
 			var recipeName = this.get('recipeName');
-			var chefName = this.get('chefName');
+			var recipeAuthor = this.get('chefName');
 			var recipeType = this.get('recipeType');
 
 			var prepTime = this.get('prepTime');
 			var cookTime = this.get('cookTime');
 			var cookTemp = this.get('cookTemp');
-			var recipeYield = this.get('recipeYield');
-			var recipeProduct = this.get('recipeProduct');
+			var isF = this.get('this.currentDenom.isF');
+			var recipeYieldAmt = this.get('recipeYieldAmt');
+			var recipeYieldType = this.get('recipeYieldType');
 			var ingredientAmount = this.get('ingredientAmount');
 			var ingredientType = this.get('ingredientType');
 			var directions = this.get('directions');
-			var chefNotes = this.get('chefNotes');
+			var recipeNotes = this.get('recipeNotes');
 
+// console.log(this.currentDenom.isF + " :currentDenom");
 
 
 
 		var recipe = {
 			recipeName: recipeName,
-			chefName: chefName,
+			recipeAuthor: recipeAuthor,
 			recipeType: recipeType,
 			prepTime: prepTime,
 			cookTime: cookTime,
 			cookTemp: cookTemp,
-			recipeYield: recipeYield,
-			recipeProduct: recipeProduct,
+			isF: isF,
+			recipeYieldAmt: recipeYieldAmt,
+			recipeYieldType: recipeYieldType,
 			ingredientAmount: ingredientAmount,
 			ingredientType: ingredientType,
 			directions: directions,
-			chefNotes: chefNotes
+			recipeNotes: recipeNotes
 
 		};
 
